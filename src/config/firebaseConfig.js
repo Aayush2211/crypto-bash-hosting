defc895a1bb57bbff02ed571800b158e91a9383b
Aayush2211 <aayush22pat@gmail.com>
2022-12-25 16:07:30 +0530

    initialise project

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDEpOOfB-9_oyXDW7QR2QrFkMw12XMyGdI",
+    authDomain: "crypto-bash.firebaseapp.com",
+    projectId: "crypto-bash",
+    storageBucket: "crypto-bash.appspot.com",
+    messagingSenderId: "216616379552",
+    appId: "1:216616379552:web:3f85fd65b88fcadcc615c5"
+  };
+
+  export default firebaseConfig;
